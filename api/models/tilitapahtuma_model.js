e6e0dc1f02b72c6437ec7146cbc3fba74ba774b9
MarsalkkaSandels <sanzuri91@gmail.com>
2022-04-04 14:01:48 +0300

    korjattu testien virheet

--- api/models/tilitapahtuma_model.js
+++ api/models/tilitapahtuma_model.js
@@ -1,10 +1,10 @@
 const db = require('../database')
-const aikakone = require('../custom_modules/time_machine')                        //Tämä kone hallitsee aikaa ja aikaleimoja
 
+//Aikaleiman muoto on YYYY-MM-DDTHH:MM:SS.000Z
 
 const tilitapahtuma = {
   get: function (callback) {
-    return db.query('select * from tilitapahtuma', callback)                    //Haetaan kaikki tapahtumat listausta varten
+    return db.query('select * from tilitapahtuma', callback)                     //Haetaan kaikki tapahtumat listausta varten
   },
   getByTili: function (idKortti, callback) {                                     //Haetaan kaikki korttiin liittyvät tapahtumat esittämistä varten
     return db.query('select * from tilitapahtuma where idKortti=? inner join kortti on tilitapahtuma.idKortti = kortti.id',
@@ -17,16 +17,16 @@ const tilitapahtuma = {
   getById: function (id, callback) {                                              //Haetaan yksittäinen tapahtuma id:n mukaan
     return db.query('select * from tilitapahtuma where id=?', [id], callback)
   },
-  add: function (tilitapahtuma, callback) {                                      //Tässä operaatio jos asiakas nostaa, lisää tai siirtää rahaa
-    return db.query('insert into tilitapahtuma (idKortti, aikaleima, tapahtuma, summa) values(?,?,?,?)',
-      [tilitapahtuma.idKortti, aikakone.TIMESTAMP, tilitapahtuma.tapahtuma, tilitapahtuma.summa], callback)
+  add: function (tilitapahtuma, callback) {                                       //Tässä operaatio jos asiakas nostaa, lisää tai siirtää rahaa
+    return db.query('insert into tilitapahtuma (idKortti, aikaleima, tapahtuma, summa) values(?,NOW(),?,?)',
+      [tilitapahtuma.idKortti, tilitapahtuma.tapahtuma, tilitapahtuma.summa], callback)
   },
   delete: function (id, callback) {                                               //Voihan tätä käyttää tapahtumien peruuttamiseen
     return db.query('delete from tilitapahtuma where id=?', [id], callback)
   },
-  update: function (id, tilitapahtuma, callback) {                               //Tämä ei tule näkymään loppukäyttäjälle
-    return db.query('update tilitapahtuma set idKortti=?, aikaleima=?, tapahtuma=?, summa=? where id=?',
-      [tilitapahtuma.idKortti, aikakone.TIMESTAMP, tilitapahtuma.tapahtuma, tilitapahtuma.summa, id], callback)
+  update: function (id, tilitapahtuma, callback) {                                //Tämä ei tule näkymään loppukäyttäjälle
+    return db.query('update tilitapahtuma set idKortti=?, aikaleima=NOW(), tapahtuma=?, summa=? where id=?',
+      [tilitapahtuma.idKortti, tilitapahtuma.tapahtuma, tilitapahtuma.summa, id], callback)
   }
 }
 
