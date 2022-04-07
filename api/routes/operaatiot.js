24a6362b5436ce624ea15bc862323d31e273446a
Esa Salminen <esa_salminen@hotmail.com>
2022-04-07 14:02:03 +0300

    operaatio model täydennys

--- api/routes/operaatiot.js
+++ api/routes/operaatiot.js
@@ -1,4 +1,3 @@
-const { response } = require('express')
 const express = require('express')
 const router = express.Router()
 const operaatiot = require('../models/operaatiot_model')
@@ -19,7 +18,7 @@ router.post('/tilitapahtumat/', function(request, response) {
 })
 
 router.post('/saldo/', function(request, response) {
-  const params = { cardNumber } = request.body;
+  const { cardNumber } = request.body;
   if(!cardNumber) {
       console.log("cardnumber missing");
       response.send(false);
@@ -29,6 +28,36 @@ router.post('/saldo/', function(request, response) {
       response.json(err);
     } else {
       response.json(dbResult[0]);
+    }
+  })
+})
+
+router.post('/nosto/', function(request, response) {
+  const params = { cardNumber, amount } = request.body;
+  if(!cardNumber || !amount) {
+      console.log("withdraw parameters missing");
+      response.send(false);
+  }
+  operaatiot.createWithdraw(params, function(err, dbResult) {
+    if (err) {
+      response.json(err);
+    } else {
+      response.json(dbResult);
+    }
+  })
+})
+
+router.post('/siirto/', function(request, response) {
+  const params = { cardNumber, amount, targetCardNumber } = request.body;
+  if(!cardNumber || !amount || !targetCardNumber) {
+      console.log("transaction parameters missing");
+      response.send(false);
+  }
+  operaatiot.createTransaction(params, function(err, dbResult) {
+    if (err) {
+      response.json(err);
+    } else {
+      response.json(dbResult);
     }
   })
 })
