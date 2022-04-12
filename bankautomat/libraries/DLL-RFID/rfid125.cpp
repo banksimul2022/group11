87d4112a27b151a00367388c15a67a87ee335480
Jan-Henrik <44895808+Jan5u@users.noreply.github.com>
2022-04-12 19:16:58 +0300

    Revert "22 kortinlukijalle muutama päivitys"

--- bankautomat/libraries/DLL-RFID/rfid125.cpp
+++ bankautomat/libraries/DLL-RFID/rfid125.cpp
@@ -4,38 +4,28 @@
 RFID125::RFID125(QObject *parent) : QObject (parent)
 {
     // allocate space in memory
-    m_serialPort = new QSerialPort();
-    m_serialPortInfo = new QSerialPortInfo();
+    m_serialPort = new QSerialPort(this);
 }
 
 RFID125::~RFID125()
 {
     disconnect(m_serialPort, SIGNAL(readyRead()), this, SLOT(receiveCardID()));
     delete m_serialPort;
-    delete m_serialPortInfo;
 }
 
 void RFID125::readCardID()
 {
-    Q_FOREACH(QSerialPortInfo port, QSerialPortInfo::availablePorts()) {
-        if (port.serialNumber() == "OL4E52F0053ECAD") *m_serialPortInfo = port;
-    }
-    m_serialPort->setPort(*m_serialPortInfo);
+    m_serialPort->setPortName("COM4");
     m_serialPort->open(QIODevice::ReadOnly);
     connect(m_serialPort, SIGNAL(readyRead()), this, SLOT(receiveCardID()));
     qDebug() << "Waiting for bankcard...";
 }
 
-void RFID125::testCardID()
-{
-    emit sendToExe(QByteArray("05009B3106"));
-}
 
 void RFID125::receiveCardID()
 {
     qDebug() << "Received data from Reader";
     QByteArray ID = m_serialPort->read(16).chopped(3).remove(0, 3);
     disconnect(m_serialPort, SIGNAL(readyRead()), this, SLOT(receiveCardID()));
-    m_serialPort->close();
     emit sendToExe(ID);
 }
