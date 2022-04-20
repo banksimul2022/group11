f7493bfe0636b6de44eabb586e222f8df0bbf5ca
Esa Salminen <esa_salminen@hotmail.com>
2022-04-20 14:17:43 +0300

    api_authentication: token verify against cardnumber

--- api/custom_modules/authentication.js
+++ api/custom_modules/authentication.js
@@ -6,15 +6,10 @@ function authenticateToken(req, res, next) {
     const token = authHeader && authHeader.split(' ')[1]
   
     console.log("token = "+token);
-    if (token == null) return res.status(401).send( {error: "Unauthorized access. Token is null"} );
-  
-    jwt.verify(token, process.env.MY_TOKEN, (err, user) => {
-      console.log(err)
-  
-      if (err) return res.status(403).send( {error: "Unauthorized access. Token is invalid"} );
-  
-      req.user = user
-      console.log(user);
+    if (!token) return res.status(401).send( {error: "Unauthorized access. Token is null"} );
+
+    jwt.verify(token, process.env.MY_TOKEN, (err, decodedToken) => {  
+      if (err || req.body.cardNumber !== decodedToken.cardNumber) return res.status(403).send( {error: "Unauthorized access. Token is invalid"} );
       next()
     })
   }
