1f8da2ab5bc4750ee13cc2b679b4e7e4e956be6b
Esa Salminen <esa_salminen@hotmail.com>
2022-04-24 10:02:35 +0300

    comments added to clarify logic

--- api/app.js
+++ api/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const authenticateToken = require('./custom_modules/authentication')
@@ -19,7 +18,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(logger('dev'));
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 
 app.use('/login', loginRouter);
 app.use(authenticateToken);
@@ -34,16 +32,5 @@ const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' });
 }
 app.use(unknownEndpoint);
-
-const errorHandler = (error, request, response, next) => {
-    console.error(error.message);
-  
-    if (error.name === 'CastError') {
-      return response.status(400).send({ error: 'malformatted id' });
-    }
-  
-    next(error)
-  }
-app.use(errorHandler);
 
 module.exports = app;
--- api/custom_modules/authentication.js
+++ api/custom_modules/authentication.js
@@ -1,14 +1,18 @@
 const jwt = require('jsonwebtoken');
 
-
 function authenticateToken(req, res, next) {
+
+    // Extract token from header
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
-  
     console.log("token = "+token);
+    
+    // Return error if token is null
     if (!token) return res.status(401).send( {error: "Unauthorized access. Token is null"} );
 
+
     jwt.verify(token, process.env.MY_TOKEN, (err, decodedToken) => {  
+      // Return error if token does not match cardnumber
       if (err || req.body.cardNumber !== decodedToken.cardNumber) return res.status(403).send( {error: "Unauthorized access. Token is invalid"} );
       next()
     })
